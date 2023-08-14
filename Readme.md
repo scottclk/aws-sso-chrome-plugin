096e707b18d6526103925558814eabccc26a2b10
Scott Clark <scott.clark@freeagent.com>
2023-08-14 08:44:27 +0100

    Move to a retry loop rather than a fixed sleep wait

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # AWS SSO Account Name Chrome Plugin
 
 Simple chrome extension to display the AWS Account Name when using SSO.
+
+Note: If the account does not have an alias set it will show as its account number
--- content.js
+++ content.js
@@ -1,8 +1,15 @@
-const main = async () => {
-  // Need to ensure the DOM has loaded fully
-  // TODO: Make this more dynamic.
-  await new Promise((r) => setTimeout(r, 1000));
+async function retryFunctionWithDelay(func, maxAttempts, delay) {
+  for (let attempts = 0; attempts < maxAttempts; attempts++) {
+    try {
+      await func();
+      break; // If successful, break out of the loop
+    } catch (error) {
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+  }
+}
 
+async function getAccountName() {
   const source = document.querySelector(
     '[data-testid="awsc-nav-account-menu-button"] span'
   );
@@ -11,7 +18,11 @@ const main = async () => {
     const role = source.textContent;
     const accountName = source.title.match(/[^ ]*$/)[0];
     source.textContent = `${role} \t (${accountName})`;
+  } else {
+    throw new Error("No account name found");
   }
-};
+}
 
-main();
+// The Account Name is dynamically loaded to the AWS Consolve from a script
+// so we grep the page every second for a maximum of 5 seconds to ensure its loaded.
+retryFunctionWithDelay(getAccountName, 5, 1000);
